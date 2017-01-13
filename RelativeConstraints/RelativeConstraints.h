20a979c0dc09f6889e9dc76e5a2d8f8aeabaa1ad
Mathias Nagler <mathias.nagler@me.com>
2017-01-13 13:59:05 +0100

    Adds Xcode project

---
+++ RelativeConstraints/RelativeConstraints.h
@@ -0,0 +1,19 @@
+//
+//  RelativeConstraints.h
+//  RelativeConstraints
+//
+//  Created by Mathias Nagler on 13.01.17.
+//  Copyright © 2017 Mathias Nagler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RelativeConstraints.
+FOUNDATION_EXPORT double RelativeConstraintsVersionNumber;
+
+//! Project version string for RelativeConstraints.
+FOUNDATION_EXPORT const unsigned char RelativeConstraintsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RelativeConstraints/PublicHeader.h>
+
+
