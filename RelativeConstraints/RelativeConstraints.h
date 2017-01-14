3ce6fbdce8c72a93d0f2297d832d28a9db574205
Mathias Nagler <mathias.nagler@me.com>
2017-01-14 10:48:23 +0100

    Publicly exposes MANConstraintRelationConstraint and MANSizeRelationConstraint

--- RelativeConstraints/RelativeConstraints.h
+++ RelativeConstraints/RelativeConstraints.h
@@ -17,3 +17,5 @@ FOUNDATION_EXPORT const unsigned char RelativeConstraintsVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <RelativeConstraints/PublicHeader.h>
 
 #import <RelativeConstraints/MANRelativeConstraint.h>
+#import <RelativeConstraints/MANConstraintRelationConstraint.h>
+#import <RelativeConstraints/MANSizeRelationConstraint.h>
